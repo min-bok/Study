532723795b5ad77a5152bad735b517d0cc4901e1
John Doe <leehy0782@naver.com>
2022-04-22 15:52:58 +0900

    ts의 함수 사용법 예시 추가

---
+++ TypeScript/함수/function.ts
@@ -0,0 +1,33 @@
+// 기명 함수
+// function add(x: number, y: number): number {
+//     return x + y;
+// }
+
+// // 익명 함수
+// let myAdd = function(x: number, y: number): number {
+//     return x + y;
+// }
+
+// -------------------------------------------------------
+
+// 함수 타입 작성하기
+
+// let myAdd: (x: number, y: number) => number = 
+//     function(x: number, y: number): number {
+//         return x + y;
+//     };
+
+// 위와 동일
+
+// let myAdd: (baseValue: number, increment: number) => number = 
+//     function(x: number, y:number): number {
+//         return x + y;
+// }
+
+// -------------------------------------------------------
+
+// 타입의 추론: 한쪽에만 타입이 있어도 타입을 알아낼 수 있음
+
+// let myAdd = function(x: number, y: number): number { 
+//     return  x + y; 
+// };
