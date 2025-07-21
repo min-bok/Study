5f2d50c5ec109dd5ec742be86c75be0493ef849a
min-bok <leehy0782@gmail.com>
2025-07-21 13:51:15 +0900

    �Fix: copy와 paste 기능 오류 수정 - 라이브러리 파일을 fabric.js v6에서 v5로 변경하면서 문제 발생 - 공식문서내 v5 Demo 코드 참고하여 코드 수정

--- fabric.js/js/feature/copy.js
+++ fabric.js/js/feature/copy.js
@@ -1,34 +1,31 @@
 let _clipboard;
 
 export function copy(canvas) {
-  if (!canvas.getActiveObject()) return;
-  canvas
-    .getActiveObject()
-    .clone()
-    .then((cloned) => {
-      _clipboard = cloned;
-    });
+  canvas.getActiveObject().clone(function (cloned) {
+    _clipboard = cloned;
+  });
 }
 
 export async function paste(canvas) {
-  const clonedObj = await _clipboard.clone();
-  canvas.discardActiveObject();
-  clonedObj.set({
-    left: clonedObj.left + 10,
-    top: clonedObj.top + 10,
-    evented: true,
-  });
-  if (clonedObj instanceof fabric.ActiveSelection) {
-    clonedObj.canvas = canvas;
-    clonedObj.forEachObject((obj) => {
-      canvas.add(obj);
+  _clipboard.clone(function (clonedObj) {
+    canvas.discardActiveObject();
+    clonedObj.set({
+      left: clonedObj.left + 10,
+      top: clonedObj.top + 10,
+      evented: true,
     });
-    clonedObj.setCoords();
-  } else {
-    canvas.add(clonedObj);
-  }
-  _clipboard.top += 10;
-  _clipboard.left += 10;
-  canvas.setActiveObject(clonedObj);
-  canvas.requestRenderAll();
+    if (clonedObj.type === "activeSelection") {
+      clonedObj.canvas = canvas;
+      clonedObj.forEachObject(function (obj) {
+        canvas.add(obj);
+      });
+      clonedObj.setCoords();
+    } else {
+      canvas.add(clonedObj);
+    }
+    _clipboard.top += 10;
+    _clipboard.left += 10;
+    canvas.setActiveObject(clonedObj);
+    canvas.requestRenderAll();
+  });
 }
