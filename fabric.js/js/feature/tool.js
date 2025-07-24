d742661d3c75936548e5dbcf2a340540d9ff5a1c
min-bok <leehy0782@gmail.com>
2025-07-24 18:19:23 +0900

    ✨Feat: 선, 화살표 만들기 기능 추가

--- fabric.js/js/feature/tool.js
+++ fabric.js/js/feature/tool.js
@@ -16,6 +16,16 @@ document.querySelector(".tool-wrap .circle").addEventListener("click", () => {
   createCircle(canvas);
 });
 
+document.querySelector(".tool-wrap .line").addEventListener("click", () => {
+  console.log("선 버튼 클릭됨");
+  createLineWithPath(canvas);
+});
+
+document.querySelector(".tool-wrap .arrow").addEventListener("click", () => {
+  console.log("화살표 버튼 클릭됨");
+  createArrowWithPath(canvas);
+});
+
 function createTextbox(canvas) {
   const text = new fabric.IText("Fabric.JS", {
     fontFamily: "Pretendard",
@@ -56,6 +66,7 @@ function createRect(canvas) {
     top: Math.random() * 500,
     width: 100,
     height: 100,
+    stroke: "blue",
     fill: "powderblue",
     opacity: 1,
   });
@@ -71,10 +82,54 @@ function createCircle(canvas) {
     left: Math.random() * 500,
     top: Math.random() * 500,
     radius: 50,
+    stroke: "red",
     fill: "pink",
     opacity: 1,
   });
 
   canvas.add(circle);
   canvas.setActiveObject(circle);
+}
+
+/** 선 만들기 */
+function createLineWithPath(canvas) {
+  const linePath = new fabric.Path(
+    `
+  M 50 50 
+  L 200 50
+`,
+    {
+      stroke: "red",
+      strokeWidth: 3,
+      fill: "", // 채우지 않음
+      selectable: true,
+    }
+  );
+
+  canvas.add(linePath);
+  canvas.setActiveObject(linePath);
+}
+
+/** 화살표 만들기 */
+function createArrowWithPath(canvas) {
+  const pathData = `
+  M 0 0 
+  L 100 0     <!-- 직선 화살대 -->
+  M 100 0 
+  L 90 -10    <!-- 화살촉 왼쪽 -->
+  M 100 0 
+  L 90 10     <!-- 화살촉 오른쪽 -->
+`;
+
+  const arrow = new fabric.Path(pathData, {
+    left: 100,
+    top: 100,
+    stroke: "black",
+    strokeWidth: 3,
+    fill: "", // 채우지 않음
+    selectable: true,
+  });
+
+  canvas.add(arrow);
+  canvas.setActiveObject(arrow);
 }
